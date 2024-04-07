6fa50d47965926a31dc5d9f79a3ef52d1011a3ef
Inaam Ahmed <inaam1817@gmail.com>
2024-04-07 18:24:23 +0500

    some editing

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -955,3 +955,13 @@ jQuery(window).on('load', function () {
 
     })(jQuery);
 });
+
+
+// service js button hide and show program
+function showSection(section){
+    var sections = document.getElementsByClassName('section');
+    for (var i = 0; i < sections.length; i++) {
+        sections[i].style.display = 'none';
+    }
+    document.getElementById(section).style.display = 'block';
+}
